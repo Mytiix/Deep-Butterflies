06596eade3d215fcd299c84c2acbd5adea0bf8a8
Louis Marganne <margannelouis@gmail.com>
2022-06-05 19:48:45 +0200

    Clean up

--- Results Code/DL/landmark_HM_models.py
+++ Results Code/DL/landmark_HM_models.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 13 12:47:43 2021
 
-@author: Navdeep Kumar
-"""
+#
+# * Copyright (c) 2009-2016. Authors: see NOTICE file.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+
+__author__ = "Navdeep Kumar <nkumar@uliege.be>"
+
+
 from tensorflow.keras.layers import (
                                      Conv2D,
                                      Conv2DTranspose,
@@ -64,6 +79,8 @@ def FCN8(input_shape, H, W, nKeypoints):
     model = Model(inputs=input, outputs=output, name="FCN8")
 
     return model
+
+
 #========================= UNet Architecture ==================================
 def UNET(input_shape, H, W, nKeypoints):
     def downsample_block(x, block_num, n_filters, pooling_on=True):
